11db906f0240651a4f76486258dff1ae95212ec7
hokhanh <hokhanha2@gmail.com>
2021-01-18 23:21:43 +0700

    project structure

---
+++ src/main/java/service/Temp.java
@@ -0,0 +1,17 @@
+package service;
+
+import dao.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author khanh
+ */
+public class Temp {
+    
+}
---
+++ src/main/java/view/Temp.java
@@ -0,0 +1,17 @@
+package view;
+
+import dao.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author khanh
+ */
+public class Temp {
+    
+}
